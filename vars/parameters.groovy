ae9a85353ee2765f490045157459af21184b7a22
Rumen Lishkov <airdata@abv.bg>
2022-11-25 07:14:02 +0200

    Add search docker tags in nexus

---
+++ vars/parameters.groovy
@@ -0,0 +1,62 @@
+def call() {
+    properties([
+        parameters([
+            string(
+                defaultValue: 'main',
+                description: 'Select the branch to deploy from',
+                name: 'BRANCH'),
+            choice(
+                choices: ['dev', 'test','prod'],
+                description: 'Select the environment to deploy',
+                name: 'ENV'),
+                [
+                $class: 'ChoiceParameter',
+                choiceType: 'PT_SINGLE_SELECT',
+                filterLength: 1,
+                filterable: false,
+                name: 'VER',
+                randomName: 'choice-parameter-1898886208177958',
+                script:
+                [
+                    $class: 'GroovyScript',
+                    fallbackScript: [
+                        classpath: [],
+                        sandbox: false,
+                        script: ''],
+                        script: [
+                            classpath: [],
+                            sandbox: false,
+                            script: '''import groovy.json.JsonSlurper
+    def nexusURL = "http://172.17.0.3:8081/repository/docker/v2/repository/docker/alpine/tags/list"
+    def nexusAPIResponse = new URL(nexusURL).text;
+    def nexusAPIResponseSlurper = [:]
+    nexusAPIResponseSlurper = new JsonSlurper().parseText(nexusAPIResponse)
+    def continuationToken = nexusAPIResponseSlurper.continuationToken
+
+    def image_tag_list = []
+    nexusAPIResponseSlurper.items.each { tag_metadata ->
+    image_tag_list.add(tag_metadata.version)
+    }
+
+    try {
+        while(continuationToken != 'null'){
+        def nexusAPIResponseWithToken = new URL("${nexusURL}&continuationToken=${continuationToken}").text;
+        println nexusAPIResponseWithToken
+        def nexusAPISlurperWithToken = [:]
+        def nexusAPIResponseSlurperWithToken = new JsonSlurper().parseText(nexusAPIResponseWithToken)
+        continuationToken = nexusAPIResponseSlurperWithToken.continuationToken
+        nexusAPIResponseSlurperWithToken.items.each { tag_metadata ->
+        image_tag_list.add(tag_metadata.version)
+        }
+        }
+    }
+    catch(Exception e){
+        println(e)
+    }
+    return image_tag_list'''
+            ]
+          ]
+        ]
+      ])
+    ])
+}
