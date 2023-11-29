298a228ebe925794d3748e3ca8df929d9e90cf37
Rumen Lishkov <airdata@abv.bg>
2023-11-29 05:31:14 +0200

    Update gitApi.groovy

--- vars/gitApi.groovy
+++ vars/gitApi.groovy
@@ -1,13 +1,18 @@
 def getPrOpen(String credsID, String gitUserRepo, String repoName){
 
+      def parts = env.CHANGE_URL.split("/")
+      def repo = parts[3]
+      def repoName = parts[4]
+      def pullRequestNumber = parts[6]
+      def GitHubApiPR = "https://api.github.com/repos/${repo}/$repoName/pulls/${pullRequestNumber}"
+      
       withCredentials([usernamePassword(credentialsId: credsID, usernameVariable: 'gitUser', passwordVariable: 'gitPwd')]) {
           // Your build steps here
           if (env.CHANGE_ID && env.CHANGE_TARGET) {
               echo "This build is triggered by a pull request."
-              echo "$credsID +++ ${gitUserRepo}" 
               // Check if the pull request is open
               def prStatus = sh(script: "curl -s -H \"Authorization: Bearer \$gitPwd\" " +
-                  "\"https://api.github.com/repos/${gitUserRepo}/${repoName}/pulls/\$CHANGE_ID\" | jq -r .state",
+                  "$GitHubApiPR" | jq -r .state",
                   returnStdout: true).trim()
               
               if (prStatus == "open") {
